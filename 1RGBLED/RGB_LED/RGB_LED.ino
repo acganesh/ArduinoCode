c7987ecd0467844f92444c334df0de2a5f604bd4
A G <acganesh@stanford.edu>
2013-05-25 13:17:06 -0500

    Added comments

--- 1RGBLED/RGB_LED/RGB_LED.ino
+++ 1RGBLED/RGB_LED/RGB_LED.ino
@@ -4,26 +4,28 @@ void setup(){
 }
 
 void loop(){
-  Esplora.writeRGB(255,0,0); // Writing the LED TO RED
-  delay(500);
-  Esplora.writeRGB(0,255,0);
-  delay(500);
-  Esplora.writeRGB(0,0,255);
+  Esplora.writeRGB(255,0,0); // Make LED Red
+  delay(500); //Wait 1/2 second (500 ms)
+  
+  Esplora.writeRGB(0,255,0); // Make LED Green
+  delay(500); // Wait 1/2 second (500 ms)
+  
+  Esplora.writeRGB(0,0,255); // Make LED Blue 
+  delay(500); // etc.
+  
+  Esplora.writeRGB(255,255,0); // Make LED Red+Green = Yellow
   delay(500);
   
-  Esplora.writeRGB(255,255,0);
+  Esplora.writeRGB(0,255,255); // Make LED Green+Blue = Light Blue
   delay(500);
   
-  Esplora.writeRGB(0,255,255);
+  Esplora.writeRGB(255,0,255); // Make LED Red+Blue = Purple
   delay(500);
   
-  Esplora.writeRGB(255,0,255);
+  Esplora.writeRGB(255,145,0); // Make LED Custom Color: Orange
   delay(500);
   
-  Esplora.writeRGB(255,145,0);
-  delay(500);
-  
-  Esplora.writeRGB(255,255,255);
+  Esplora.writeRGB(255,255,255); // Make LED Red+Green+Blue = Write
   delay(500);
 }
 
